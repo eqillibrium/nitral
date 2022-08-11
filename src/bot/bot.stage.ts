e82df95e76f91e0cfd161463cb4dbc65e5491e34
Naerdel <eqillibrium@live.ru>
2022-08-11 13:48:37 +0300

    remove stage file

--- src/bot/bot.stage.ts
+++
@@ -1,6 +0,0 @@
-import { Scenes } from 'telegraf'
-import { IBotContext } from './bot.context.interface'
-import addressSceneInstance from './scenes/address/address.scene'
-import infoSceneInstance from './scenes/info/info.scene'
-
-export const stage = new Scenes.Stage<IBotContext>([addressSceneInstance, infoSceneInstance])
